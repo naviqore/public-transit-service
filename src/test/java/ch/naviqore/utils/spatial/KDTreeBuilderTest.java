881dd429f0dc14cf48a187b89a732c30da29359c
Lukas Connolly <lukas_connolly@hotmail.com>
2024-05-12 13:08:44 +0200

    REFACTOR: NAV-29 - Remove unused import.

--- src/test/java/ch/naviqore/utils/spatial/KDTreeBuilderTest.java
+++ src/test/java/ch/naviqore/utils/spatial/KDTreeBuilderTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
