2531356f5dbbe68934c3a04f529d0e26aad15799
Lukas Connolly <lukas_connolly@hotmail.com>
2024-05-15 22:55:58 +0200

    TEST: NAV-32 - Add more coordinate tests.

---
+++ src/test/java/ch/naviqore/utils/spatial/CartesianCoordinateTest.java
@@ -0,0 +1,88 @@
+package ch.naviqore.utils.spatial;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CartesianCoordinateTest {
+
+    private static final double TOLERANCE = 0.0001;
+
+    @Nested
+    class Constructor {
+        // Note: All positive test cases are covered in the distanceTo test cases
+        static Stream<Arguments> invalidCoordinatesProvider() {
+            return Stream.of(Arguments.of(Double.NaN, 0), Arguments.of(0, Double.NaN));
+        }
+
+        @ParameterizedTest
+        @MethodSource("invalidCoordinatesProvider")
+        void invalidArguments(double x, double y) {
+            assertThrows(IllegalArgumentException.class, () -> new CartesianCoordinate(x, y));
+        }
+    }
+
+    @Nested
+    class DistanceTo {
+
+        static Stream<Arguments> distanceToProvider() {
+            return Stream.of(Arguments.of(new CartesianCoordinate(0, 0), new CartesianCoordinate(0, 0), 0),
+                    Arguments.of(new CartesianCoordinate(0, 0), new CartesianCoordinate(1, 0), 1),
+                    Arguments.of(new CartesianCoordinate(0, 0), new CartesianCoordinate(0, 1), 1),
+                    Arguments.of(new CartesianCoordinate(0, 0), new CartesianCoordinate(1, 1), Math.sqrt(2)),
+                    Arguments.of(new CartesianCoordinate(0, 0), new CartesianCoordinate(-1, 0), 1),
+                    Arguments.of(new CartesianCoordinate(0, 0), new CartesianCoordinate(0, -1), 1),
+                    Arguments.of(new CartesianCoordinate(0, 0), new CartesianCoordinate(-1, -1), Math.sqrt(2)),
+                    Arguments.of(new CartesianCoordinate(1, 1), new CartesianCoordinate(1, 1), 0),
+                    Arguments.of(new CartesianCoordinate(1, 1), new CartesianCoordinate(2, 1), 1),
+                    Arguments.of(new CartesianCoordinate(1, 1), new CartesianCoordinate(1, 2), 1),
+                    Arguments.of(new CartesianCoordinate(1.5, 1.5), new CartesianCoordinate(1.5, 1.5), 0),
+                    Arguments.of(new CartesianCoordinate(1.5, 1.5), new CartesianCoordinate(2.5, 1.5), 1),
+                    Arguments.of(new CartesianCoordinate(1, 1), new CartesianCoordinate(-1, -1), 2 * Math.sqrt(2)));
+        }
+
+        @ParameterizedTest(name = "distance from: {0} to: {1}")
+        @MethodSource("distanceToProvider")
+        void distanceTo(CartesianCoordinate coordinate1, CartesianCoordinate coordinate2,
+                              double expectedDistance) {
+            assertEquals(expectedDistance, coordinate1.distanceTo(coordinate2), TOLERANCE);
+        }
+
+        @ParameterizedTest(name = "distance from: {0} to: {1}")
+        @MethodSource("distanceToProvider")
+        void distanceTo_withDoubleParameters(CartesianCoordinate coordinate1, CartesianCoordinate coordinate2,
+                                                  double expectedDistance) {
+            assertEquals(expectedDistance, coordinate1.distanceTo(coordinate2.getFirstComponent(),
+                    coordinate2.getSecondComponent()), TOLERANCE);
+        }
+
+        @Test
+        void distanceTo_withNull() {
+            CartesianCoordinate coordinate = new CartesianCoordinate(0, 0);
+            assertThrows(IllegalArgumentException.class, () -> coordinate.distanceTo((CartesianCoordinate) null));
+        }
+
+        @Test
+        void distanceTo_withDifferentType() {
+            CartesianCoordinate coordinate = new CartesianCoordinate(0, 0);
+            GeoCoordinate geoCoordinate = new GeoCoordinate(0, 0);
+            assertThrows(IllegalArgumentException.class, () -> coordinate.distanceTo(geoCoordinate));
+        }
+
+        @Test
+        void distanceTo_withNaN() {
+            CartesianCoordinate coordinate = new CartesianCoordinate(0, 0);
+            assertThrows(IllegalArgumentException.class, () -> coordinate.distanceTo(Double.NaN, 0));
+            assertThrows(IllegalArgumentException.class, () -> coordinate.distanceTo(0, Double.NaN));
+        }
+
+    }
+
+}
--- src/test/java/ch/naviqore/utils/spatial/GeoCoordinateTest.java
+++ src/test/java/ch/naviqore/utils/spatial/GeoCoordinateTest.java
@@ -18,7 +18,8 @@ public class GeoCoordinateTest {
     @Nested
     class Constructor {
         static Stream<Arguments> invalidCoordinatesProvider() {
-            return Stream.of(Arguments.of(-91, 0), Arguments.of(91, 0), Arguments.of(0, -181), Arguments.of(0, 181));
+            return Stream.of(Arguments.of(-91, 0), Arguments.of(91, 0), Arguments.of(0, -181), Arguments.of(0, 181),
+                    Arguments.of(Double.NaN, 0), Arguments.of(0, Double.NaN));
         }
 
         @ParameterizedTest
@@ -32,7 +33,6 @@ public class GeoCoordinateTest {
     class DistanceTo {
 
         static Stream<Arguments> distanceToProvider() {
-
             // city coordinates and distances from
             // https://www.luftlinie.org/
             GeoCoordinate zuerich = new GeoCoordinate(47.369022, 8.538033);
@@ -60,7 +60,8 @@ public class GeoCoordinateTest {
         }
 
         static Stream<Arguments> invalidCoordinatesProvider() {
-            return Stream.of(Arguments.of(-91, 0), Arguments.of(91, 0), Arguments.of(0, -181), Arguments.of(0, 181));
+            return Stream.of(Arguments.of(-91, 0), Arguments.of(91, 0), Arguments.of(0, -181), Arguments.of(0, 181),
+                    Arguments.of(Double.NaN, 0), Arguments.of(0, Double.NaN));
         }
 
         @ParameterizedTest(name = "distance from: {1} to: {3}")
@@ -89,6 +90,46 @@ public class GeoCoordinateTest {
             String msg = String.format("distance from %s to %s should be %f", d1, d2, expected);
             assertEquals(expected, c1.distanceTo(c2.latitude(), c2.longitude()), expected * TOLERANCE, msg);
         }
+
+        @Test
+        void withDifferentType() {
+            GeoCoordinate c = new GeoCoordinate(47.368650, 8.539183);
+            CartesianCoordinate cc = new CartesianCoordinate(0, 0);
+            assertThrows(IllegalArgumentException.class, () -> c.distanceTo(cc));
+        }
+
+    }
+
+    @Nested
+    class CompareTo {
+
+        static Stream<Arguments> compareToArguments(){
+            // allowed discrepancy between coordinates
+            double epsilon = 1e-5;
+            return Stream.of(
+                    // Equal cases
+                    Arguments.of(new GeoCoordinate(0, 0), new GeoCoordinate(0, 0), 0),
+                    Arguments.of(new GeoCoordinate(0, 0), new GeoCoordinate(0, -epsilon), 0),
+                    Arguments.of(new GeoCoordinate(0, 0), new GeoCoordinate(0, epsilon), 0),
+                    Arguments.of(new GeoCoordinate(0, 0), new GeoCoordinate(epsilon, 0), 0),
+                    Arguments.of(new GeoCoordinate(0, 0), new GeoCoordinate(-epsilon, 0), 0),
+                    // Greater than cases
+                    Arguments.of(new GeoCoordinate(0, 0), new GeoCoordinate(0, -1), 1),
+                    Arguments.of(new GeoCoordinate(0, 0), new GeoCoordinate(-1, 0), 1),
+                    Arguments.of(new GeoCoordinate(0, 0), new GeoCoordinate(-1, 1), 1),
+                    // Smaller than cases
+                    Arguments.of(new GeoCoordinate(0, 0), new GeoCoordinate(0, 1), -1),
+                    Arguments.of(new GeoCoordinate(0, 0), new GeoCoordinate(1, 0), -1),
+                    Arguments.of(new GeoCoordinate(0, 0), new GeoCoordinate(1, -1), -1)
+            );
+        }
+
+        @ParameterizedTest(name = "compare {0} to {1}")
+        @MethodSource("compareToArguments")
+        void compareTo(GeoCoordinate c1, GeoCoordinate c2, double expected) {
+            assertEquals(expected, c1.compareTo(c2));
+        }
+
 
     }
 
