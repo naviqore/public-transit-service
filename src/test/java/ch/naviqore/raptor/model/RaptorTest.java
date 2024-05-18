ba1f0cfc175126beb120bf80548208f10722ff22
Lukas Connolly <lukas_connolly@hotmail.com>
2024-05-18 09:54:07 +0200

    TEST: NAV-17 - Refactor test utilities to add more versatility to raptor build for testing.

--- src/test/java/ch/naviqore/raptor/model/RaptorTest.java
+++ src/test/java/ch/naviqore/raptor/model/RaptorTest.java
@@ -1,6 +1,5 @@
 package ch.naviqore.raptor.model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -42,64 +41,83 @@ import java.util.Set;
  */
 class RaptorTest {
 
-    private static final int DAY_START = 5 * 60 * 60;
-    private static final int DAY_END = 25 * 60 * 60;
-    private static final int TRAVEL_TIME = 60 * 5;
-    private static final int DWELL_TIME = 60 * 2;
-    private static final List<Route> ROUTES = List.of(
-            new Route("R1", List.of("A", "B", "C", "D", "E", "F", "G"), 15 * 60, 60),
-            new Route("R2", List.of("H", "B", "I", "J", "K", "L"), 30 * 60, 5 * 60),
-            new Route("R3", List.of("M", "K", "N", "O", "P", "Q"), 15 * 60, 7 * 60),
-            new Route("R4", List.of("R", "P", "F", "S"), 60 * 60, 0));
-    private static final List<Transfer> TRANSFERS = List.of(new Transfer("N", "D", 60 * 10),
-            new Transfer("L", "R", 60 * 3));
-    private Raptor raptor;
+    private static final int SECONDS_IN_HOUR = 3600;
+    private static final int DAY_START_HOUR = 5;
+    private static final int DAY_END_HOUR = 25;
+    private static final List<Utilities.Route> ROUTES = List.of(
+            new Utilities.Route("R1", List.of("A", "B", "C", "D", "E", "F", "G")),
+            new Utilities.Route("R2", List.of("H", "B", "I", "J", "K", "L")),
+            new Utilities.Route("R3", List.of("M", "K", "N", "O", "P", "Q")),
+            new Utilities.Route("R4", List.of("R", "P", "F", "S")));
+    private static final List<Utilities.Transfer> TRANSFERS = List.of(new Utilities.Transfer("N", "D", 60),
+            new Utilities.Transfer("L", "R", 30));
 
-    @BeforeEach
-    void setUp() {
-        Set<String> addedStops = new HashSet<>();
-        RaptorBuilder builder = Raptor.builder();
-        ROUTES.forEach(route -> {
-            builder.addRoute(route.id + "-F");
-            builder.addRoute(route.id + "-R");
-            route.stops.forEach(stop -> {
-                if (!addedStops.contains(stop)) {
-                    builder.addStop(stop);
-                    addedStops.add(stop);
+    static class Utilities {
+
+        public static Raptor buildRaptor() {
+            return buildRaptor(ROUTES, TRANSFERS, DAY_START_HOUR, DAY_END_HOUR);
+        }
+
+        public static Raptor buildRaptor(List<Route> routes, List<Transfer> transfers, int dayStart, int dayEnd) {
+            Set<String> addedStops = new HashSet<>();
+            RaptorBuilder builder = Raptor.builder();
+            routes.forEach(route -> {
+                builder.addRoute(route.id + "-F");
+                builder.addRoute(route.id + "-R");
+                route.stops.forEach(stop -> {
+                    if (!addedStops.contains(stop)) {
+                        builder.addStop(stop);
+                        addedStops.add(stop);
+                    }
+                });
+                for (int i = 0; i < route.stops.size(); i++) {
+                    builder.addRouteStop(route.stops.get(i), route.id + "-F");
+                    builder.addRouteStop(route.stops.get(route.stops.size() - 1 - i), route.id + "-R");
+                }
+                int time = dayStart * SECONDS_IN_HOUR + route.firstDepartureOffsetInMinutes * 60;
+                while (time < dayEnd * SECONDS_IN_HOUR) {
+                    int departureTime = time;
+                    // first stop of trip has no arrival time
+                    int arrivalTime = 0;
+                    for (int i = 0; i < route.stops.size(); i++) {
+                        if (i + 1 == route.stops.size()) {
+                            // last stop of trip has no departure time
+                            departureTime = 0;
+                        }
+                        builder.addStopTime(route.stops.get(i), route.id + "-F", arrivalTime, departureTime);
+                        builder.addStopTime(route.stops.get(route.stops.size() - 1 - i), route.id + "-R", arrivalTime,
+                                departureTime);
+
+                        arrivalTime = departureTime + route.timeBetweenStopsInMinutes * 60;
+                        departureTime = arrivalTime + route.dwellTimeInMinutes * 60;
+                    }
+                    time += route.timeBetweenDeparturesInMinutes * 60;
                 }
             });
-            for (int i = 0; i < route.stops.size(); i++) {
-                builder.addRouteStop(route.stops.get(i), route.id + "-F");
-                builder.addRouteStop(route.stops.get(route.stops.size() - 1 - i), route.id + "-R");
-            }
-            int time = DAY_START + route.offset;
-            while (time < DAY_END) {
-                for (int i = 0; i < route.stops.size(); i++) {
-                    builder.addStopTime(route.stops.get(i), route.id + "-F", time, time + TRAVEL_TIME);
-                    builder.addStopTime(route.stops.get(route.stops.size() - 1 - i), route.id + "-R", time,
-                            time + TRAVEL_TIME);
-                    time += TRAVEL_TIME + DWELL_TIME;
-                }
-            }
-        });
-        TRANSFERS.forEach(transfer -> {
-            builder.addTransfer(transfer.sourceStop, transfer.targetStop, transfer.duration);
-            builder.addTransfer(transfer.targetStop, transfer.sourceStop, transfer.duration);
-        });
-        raptor = builder.build();
-    }
+            transfers.forEach(transfer -> {
+                builder.addTransfer(transfer.sourceStop, transfer.targetStop, transfer.durationInMinutes * 60);
+                builder.addTransfer(transfer.targetStop, transfer.sourceStop, transfer.durationInMinutes * 60);
+            });
+            return builder.build();
+        }
 
-    record Route(String id, List<String> stops, int headway, int offset) {
-    }
+        record Route(String id, List<String> stops, int firstDepartureOffsetInMinutes,
+                     int timeBetweenDeparturesInMinutes, int timeBetweenStopsInMinutes, int dwellTimeInMinutes) {
+            public Route(String id, List<String> stops) {
+                this(id, stops, 0, 15, 5, 1);
+            }
+        }
 
-    record Transfer(String sourceStop, String targetStop, int duration) {
+        record Transfer(String sourceStop, String targetStop, int durationInMinutes) {
+        }
     }
 
     @Nested
     class EarliestArrival {
         @Test
-        void testRoutingBetweenIntersectingRoutes() {
-            List<Raptor.Connection> connections = raptor.routeEarliestArrival("A", "Q", 8 * 60 * 60);
+        void routingBetweenIntersectingRoutes() {
+            Raptor raptor = Utilities.buildRaptor();
+            List<Raptor.Connection> connections = raptor.routeEarliestArrival("A", "Q", 8 * SECONDS_IN_HOUR);
             System.out.println(connections);
             // TODO: assertThat...
         }
