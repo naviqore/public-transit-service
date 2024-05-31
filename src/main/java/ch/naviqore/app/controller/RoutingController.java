44caf286709fb5d431637d4c15672d888ca20193
Lukas Connolly <lukas_connolly@hotmail.com>
2024-05-31 11:49:00 +0200

    ENH: NAV-84 - Add lat/lon pair as alternative to stopId to routing request parameters.

--- src/main/java/ch/naviqore/app/controller/RoutingController.java
+++ src/main/java/ch/naviqore/app/controller/RoutingController.java
@@ -4,6 +4,7 @@ import ch.naviqore.app.dto.Connection;
 import ch.naviqore.app.dto.DtoDummyData;
 import ch.naviqore.app.dto.EarliestArrival;
 import ch.naviqore.service.ConnectionRoutingService;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,12 +26,31 @@ public class RoutingController {
     }
 
     @GetMapping("/connections")
-    public List<Connection> getConnections(@RequestParam String sourceStopId, @RequestParam String targetStopId,
+    public List<Connection> getConnections(@RequestParam(required = false) String sourceStopId,
+                                           @RequestParam(required = false, defaultValue = "-1.0") double sourceLatitude,
+                                           @RequestParam(required = false, defaultValue = "-1.0") double sourceLongitude,
+                                           @RequestParam(required = false) String targetStopId,
+                                           @RequestParam(required = false, defaultValue = "-1.0") double targetLatitude,
+                                           @RequestParam(required = false, defaultValue = "-1.0") double targetLongitude,
                                            @RequestParam(required = false) LocalDateTime departureDateTime,
                                            @RequestParam(required = false, defaultValue = "2147483647") int maxWalkingDuration,
                                            @RequestParam(required = false, defaultValue = "2147483647") int maxTransferNumber,
                                            @RequestParam(required = false, defaultValue = "2147483647") int maxTravelTime,
                                            @RequestParam(required = false, defaultValue = "0") int minTransferTime) {
+        if( sourceStopId == null ) {
+            if( sourceLatitude < 0 || sourceLongitude < 0 ) {
+                throw new IllegalArgumentException("Either sourceStopId or sourceLatitude and sourceLongitude must be provided.");
+            }
+            throw new NotImplementedException("Location based routing is not implemented yet.");
+        }
+
+        if (targetStopId == null) {
+            if (targetLatitude < 0 || targetLongitude < 0) {
+                throw new IllegalArgumentException("Either targetStopId or targetLatitude and targetLongitude must be provided.");
+            }
+            throw new NotImplementedException("Location based routing is not implemented yet.");
+        }
+
         // TODO: Implement the method with  maxWalkingDuration, maxTransferNumber, maxTravelTime, minTransferTime
         return DtoDummyData.getConnections(sourceStopId, targetStopId, departureDateTime);
         // TODO: Decide on location or stop id.
@@ -38,12 +58,21 @@ public class RoutingController {
     }
 
     @GetMapping("/isolines")
-    public List<EarliestArrival> getIsolines(@RequestParam String sourceStopId,
+    public List<EarliestArrival> getIsolines(@RequestParam(required = false) String sourceStopId,
+                                             @RequestParam(required = false, defaultValue = "-1.0") double sourceLatitude,
+                                             @RequestParam(required = false, defaultValue = "-1.0") double sourceLongitude,
                                              @RequestParam(required = false) LocalDateTime departureDateTime,
                                              @RequestParam(required = false, defaultValue = "2147483647") int maxWalkingDuration,
                                              @RequestParam(required = false, defaultValue = "2147483647") int maxTransferNumber,
                                              @RequestParam(required = false, defaultValue = "2147483647") int maxTravelTime,
                                              @RequestParam(required = false, defaultValue = "0") int minTransferTime) {
+        if( sourceStopId == null ) {
+            if( sourceLatitude < 0 || sourceLongitude < 0 ) {
+                throw new IllegalArgumentException("Either sourceStopId or sourceLatitude and sourceLongitude must be provided.");
+            }
+            throw new NotImplementedException("Location based routing is not implemented yet.");
+        }
+
         // TODO: Implement the method with maxWalkingDuration, maxTransferNumber, minTransferTime
         return DtoDummyData.getIsolines(sourceStopId, departureDateTime, maxTravelTime);
         // TODO: Decide on location or stop id.
