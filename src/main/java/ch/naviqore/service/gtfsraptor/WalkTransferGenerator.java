84416e65449aa94018cb0d49826cad3d736809e7
Merlin Unterfinger <info@munterfinger.ch>
2024-05-31 10:09:46 +0200

    ENH: NAV-35 - Remove concurrent linked queue and use stream api

--- src/main/java/ch/naviqore/service/gtfsraptor/WalkTransferGenerator.java
+++ src/main/java/ch/naviqore/service/gtfsraptor/WalkTransferGenerator.java
@@ -4,10 +4,8 @@ import ch.naviqore.gtfs.schedule.model.GtfsSchedule;
 import ch.naviqore.gtfs.schedule.model.Stop;
 import lombok.extern.log4j.Log4j2;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Implements a transfer generator that creates minimum time transfers between stops where the {@link GtfsSchedule} does
@@ -34,14 +32,14 @@ public class WalkTransferGenerator implements TransferGenerator {
      * Creates a new WalkTransferGenerator with the given WalkCalculator, minimum transfer time and maximum walking
      * distance.
      *
-     * @param walkCalculator WalkCalculator to use for calculating walking times.
+     * @param walkCalculator      WalkCalculator to use for calculating walking times.
      * @param minimumTransferTime Minimum transfer time between stops in seconds.
-     * @param maxWalkDistance Maximum walking distance between stops in meters.
+     * @param maxWalkDistance     Maximum walking distance between stops in meters.
      */
     public WalkTransferGenerator(WalkCalculator walkCalculator, int minimumTransferTime, int maxWalkDistance) {
-        if( walkCalculator == null ) throw new IllegalArgumentException("walkCalculator is null");
-        if( minimumTransferTime < 0 ) throw new IllegalArgumentException("minimumTransferTime is negative");
-        if( maxWalkDistance <= 0 ) throw new IllegalArgumentException("maxWalkDistance is negative or zero");
+        if (walkCalculator == null) throw new IllegalArgumentException("walkCalculator is null");
+        if (minimumTransferTime < 0) throw new IllegalArgumentException("minimumTransferTime is negative");
+        if (maxWalkDistance <= 0) throw new IllegalArgumentException("maxWalkDistance is negative or zero");
         this.walkCalculator = walkCalculator;
         this.minimumTransferTime = minimumTransferTime;
         this.maxWalkDistance = maxWalkDistance;
@@ -57,32 +55,25 @@ public class WalkTransferGenerator implements TransferGenerator {
      */
     @Override
     public List<MinimumTimeTransfer> generateTransfers(GtfsSchedule schedule) {
-
         Map<String, Stop> stops = schedule.getStops();
-        ConcurrentLinkedQueue<MinimumTimeTransfer> transfers = new ConcurrentLinkedQueue<>();
 
         log.info("Generating transfers between {} stops", stops.size());
-
-        stops.values().parallelStream().forEach(fromStop -> {
-            List<Stop> nearbyStops = schedule.getNearestStops(
-                    fromStop.getCoordinate().latitude(),
-                    fromStop.getCoordinate().longitude(),
-                    maxWalkDistance
-            );
-            nearbyStops.forEach(toStop -> createTransfer(fromStop, toStop, transfers));
-        });
-
+        List<MinimumTimeTransfer> transfers = stops.values().parallelStream().flatMap(fromStop -> {
+            List<Stop> nearbyStops = schedule.getNearestStops(fromStop.getCoordinate().latitude(),
+                    fromStop.getCoordinate().longitude(), maxWalkDistance);
+            return nearbyStops.stream()
+                    .filter(toStop -> !toStop.equals(fromStop))
+                    .map(toStop -> createTransfer(fromStop, toStop));
+        }).toList();
         log.info("Generated {} transfers between {} stops", transfers.size(), stops.size());
 
-        return new ArrayList<>(transfers);
+        return transfers;
     }
 
-    private void createTransfer(Stop fromStop, Stop toStop, ConcurrentLinkedQueue<MinimumTimeTransfer> transfers) {
-        if (!fromStop.equals(toStop)) {
-            // calculate the walking time between the stops
-            Walk walk = walkCalculator.calculateWalk(fromStop.getCoordinate(), toStop.getCoordinate());
-            int transferDuration = Math.max(walk.duration(), minimumTransferTime);
-            transfers.add(new MinimumTimeTransfer(fromStop, toStop, transferDuration));
-        }
+    private MinimumTimeTransfer createTransfer(Stop fromStop, Stop toStop) {
+        // calculate the walking time between the stops
+        Walk walk = walkCalculator.calculateWalk(fromStop.getCoordinate(), toStop.getCoordinate());
+        int transferDuration = Math.max(walk.duration(), minimumTransferTime);
+        return new MinimumTimeTransfer(fromStop, toStop, transferDuration);
     }
 }
