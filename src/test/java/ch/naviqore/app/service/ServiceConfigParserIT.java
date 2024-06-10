a64f7ab17ec4fcfb6403a19eaab7b75323d45d76
Merlin Unterfinger <info@munterfinger.ch>
2024-06-10 17:19:43 +0200

    TEST: NAV-77 - Extract constants

--- src/test/java/ch/naviqore/app/service/ServiceConfigParserIT.java
+++ src/test/java/ch/naviqore/app/service/ServiceConfigParserIT.java
@@ -8,23 +8,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ServiceConfigParserIT {
 
+    private static final String GTFS_URL = "src/test/resources/ch/naviqore/gtfs/schedule/sample-feed-1.zip";
+    private static final int MINIMUM_TRANSFER_TIME = 200;
+    private static final int MAXIMUM_WALKING_DISTANCE = 600;
+    private static final int WALKING_SPEED = 4000;
+    private static final String WALKING_CALCULATOR_TYPE = "BEE_LINE_DISTANCE";
+
     @Test
     void testServiceConfigParser_withValidInputs() {
-        String gtfsUrl = "src/test/resources/ch/naviqore/gtfs/schedule/sample-feed-1.zip";
-        int minimumTransferTime = 200;
-        int maxWalkingDistance = 600;
-        int walkingSpeed = 4000;
-        String walkingCalculatorType = "BEE_LINE_DISTANCE";
-
-        ServiceConfigParser parser = new ServiceConfigParser(gtfsUrl, minimumTransferTime, maxWalkingDistance,
-                walkingSpeed, walkingCalculatorType);
+        ServiceConfigParser parser = new ServiceConfigParser(GTFS_URL, MINIMUM_TRANSFER_TIME, MAXIMUM_WALKING_DISTANCE,
+                WALKING_SPEED, WALKING_CALCULATOR_TYPE);
 
         // check that the service config has been loaded correctly
         ServiceConfig config = parser.getServiceConfig();
-        assertEquals(gtfsUrl, config.getGtfsUrl());
-        assertEquals(minimumTransferTime, config.getMinimumTransferTime());
-        assertEquals(maxWalkingDistance, config.getMaxWalkingDistance());
-        assertEquals(walkingSpeed, config.getWalkingSpeed());
+        assertEquals(GTFS_URL, config.getGtfsUrl());
+        assertEquals(MINIMUM_TRANSFER_TIME, config.getMinimumTransferTime());
+        assertEquals(MAXIMUM_WALKING_DISTANCE, config.getMaxWalkingDistance());
+        assertEquals(WALKING_SPEED, config.getWalkingSpeed());
         assertEquals(ServiceConfig.WalkCalculatorType.BEE_LINE_DISTANCE, config.getWalkCalculatorType());
     }
 
@@ -32,8 +32,8 @@ public class ServiceConfigParserIT {
     void testServiceConfigParser_withInvalidWalkCalculatorType() {
         // check that the service config has been loaded correctly
         assertThrows(IllegalArgumentException.class,
-                () -> new ServiceConfigParser("src/test/resources/ch/naviqore/gtfs/schedule/invalidSchedule.zip", 200,
-                        600, 4000, "INVALID"));
+                () -> new ServiceConfigParser(GTFS_URL, MINIMUM_TRANSFER_TIME, MAXIMUM_WALKING_DISTANCE, WALKING_SPEED,
+                        "INVALID"));
     }
 
 }
