20190837551118e08f592887769aadc8a6e7f183
Merlin Unterfinger <info@munterfinger.ch>
2024-06-12 13:22:51 +0200

    ENH: NAV-76 - Adjust ServiceConfig to new parameters

--- src/main/java/ch/naviqore/service/config/ServiceConfig.java
+++ src/main/java/ch/naviqore/service/config/ServiceConfig.java
@@ -3,54 +3,89 @@ package ch.naviqore.service.config;
 import lombok.Getter;
 
 /**
- * Configuration to create a public transit service with default values set.
+ * Configuration to create a public transit service.
  */
 @Getter
 public class ServiceConfig {
 
-    private final int minimumTransferTime;
-    private final int maxWalkingDistance;
-    private final int walkingSpeed;
-    private final int sameStationTransferTime;
-    private final WalkCalculatorType walkCalculatorType;
-    private final String gtfsUrl;
+    // note: The defaults should match the default values in the application.properties file.
+    public static final String DEFAULT_GTFS_STATIC_UPDATE_CRON = "0 0 4 * * *";
+    public static final int DEFAULT_TRANSFER_TIME_BETWEEN_STOPS_MINIMUM = 120;
+    public static final int DEFAULT_TRANSFER_TIME_SAME_STOP_DEFAULT = 120;
+    public static final WalkCalculatorType DEFAULT_WALKING_CALCULATOR_TYPE = WalkCalculatorType.BEE_LINE_DISTANCE;
+    public static final double DEFAULT_WALKING_SPEED = 1.4;
+    public static final int DEFAULT_WALKING_DISTANCE_MAXIMUM = 500;
+    public static final int DEFAULT_WALKING_DURATION_MINIMUM = 120;
+    public static final int DEFAULT_CACHE_SIZE = 5;
+    public static final CacheEvictionStrategy DEFAULT_CACHE_EVICTION_STRATEGY = CacheEvictionStrategy.LRU;
 
-    public ServiceConfig(String gtfsUrl, int minimumTransferTime, int maxWalkingDistance, int walkingSpeed,
-                         int sameStationTransferTime, WalkCalculatorType walkCalculatorType) {
-        this.gtfsUrl = gtfsUrl;
+    private final String gtfsStaticUrl;
+    private final String gtfsStaticUpdateCron;
+    private final int transferTimeBetweenStopsMinimum;
+    private final int transferTimeSameStopDefault;
+    private final WalkCalculatorType walkingCalculatorType;
+    private final double walkingSpeed;
+    private final int walkingDistanceMaximum;
+    private final int walkingDurationMinimum;
+    private final int cacheSize;
+    private final CacheEvictionStrategy cacheEvictionStrategy;
 
-        if (minimumTransferTime < 0) {
-            throw new IllegalArgumentException("Minimum transfer time must be greater than or equal to 0.");
-        }
-        this.minimumTransferTime = minimumTransferTime;
+    public ServiceConfig(String gtfsStaticUrl, String gtfsStaticUpdateCron, int transferTimeBetweenStopsMinimum,
+                         int transferTimeSameStopDefault, WalkCalculatorType walkingCalculatorType, double walkingSpeed,
+                         int walkingDistanceMaximum, int walkingDurationMinimum, int cacheSize,
+                         CacheEvictionStrategy cacheEvictionStrategy) {
 
-        if (maxWalkingDistance < 0) {
-            throw new IllegalArgumentException("Maximum walking distance must be greater than or equal to 0.");
-        }
-        this.maxWalkingDistance = maxWalkingDistance;
-
-        if (walkingSpeed <= 0) {
-            throw new IllegalArgumentException("Walking speed must be greater than to 0.");
-        }
-        this.walkingSpeed = walkingSpeed;
-
-        if (sameStationTransferTime < 0) {
-            throw new IllegalArgumentException("Same station transfer time must be greater than or equal to 0.");
-        }
-        this.sameStationTransferTime = sameStationTransferTime;
-
-        this.walkCalculatorType = walkCalculatorType;
+        this.gtfsStaticUrl = validateNonNull(gtfsStaticUrl, "gtfsStaticUrl");
+        this.gtfsStaticUpdateCron = validateNonNull(gtfsStaticUpdateCron, "gtfsStaticUpdateCron");
+        this.transferTimeBetweenStopsMinimum = validateNonNegative(transferTimeBetweenStopsMinimum,
+                "transferTimeBetweenStopsMinimum");
+        this.transferTimeSameStopDefault = validateNonNegative(transferTimeSameStopDefault,
+                "transferTimeSameStopDefault");
+        this.walkingCalculatorType = validateNonNull(walkingCalculatorType, "walkingCalculatorType");
+        this.walkingSpeed = validatePositive(walkingSpeed, "walkingSpeed");
+        this.walkingDistanceMaximum = validateNonNegative(walkingDistanceMaximum, "walkingDistanceMaximum");
+        this.walkingDurationMinimum = validateNonNegative(walkingDurationMinimum, "walkingDurationMinimum");
+        this.cacheSize = validatePositive(cacheSize, "cacheSize");
+        this.cacheEvictionStrategy = validateNonNull(cacheEvictionStrategy, "cacheEvictionStrategy");
     }
 
     /**
      * Constructor with defaults
      */
-    public ServiceConfig(String gtfsUrl) {
-        this(gtfsUrl, 120, 500, 3500, 120, WalkCalculatorType.BEE_LINE_DISTANCE);
+    public ServiceConfig(String gtfsStaticUrl) {
+        this(gtfsStaticUrl, DEFAULT_GTFS_STATIC_UPDATE_CRON, DEFAULT_TRANSFER_TIME_BETWEEN_STOPS_MINIMUM,
+                DEFAULT_TRANSFER_TIME_SAME_STOP_DEFAULT, DEFAULT_WALKING_CALCULATOR_TYPE, DEFAULT_WALKING_SPEED,
+                DEFAULT_WALKING_DISTANCE_MAXIMUM, DEFAULT_WALKING_DURATION_MINIMUM, DEFAULT_CACHE_SIZE,
+                DEFAULT_CACHE_EVICTION_STRATEGY);
+    }
+
+    private static <T> T validateNonNull(T value, String name) {
+        if (value == null) {
+            throw new IllegalArgumentException(name + " must not be null.");
+        }
+        return value;
+    }
+
+    private static <T extends Number> T validateNonNegative(T value, String name) {
+        if (value.doubleValue() < 0) {
+            throw new IllegalArgumentException(name + " must be greater than or equal to 0.");
+        }
+        return value;
+    }
+
+    private static <T extends Number> T validatePositive(T value, String name) {
+        if (value.doubleValue() <= 0) {
+            throw new IllegalArgumentException(name + " must be greater than 0.");
+        }
+        return value;
     }
 
     public enum WalkCalculatorType {
         BEE_LINE_DISTANCE
     }
 
+    public enum CacheEvictionStrategy {
+        LRU,
+        MRU
+    }
 }
