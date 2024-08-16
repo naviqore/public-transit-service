445c59d199a952081678fc65566b51106c409513
Lukas Connolly <lukas_connolly@hotmail.com>
2024-08-16 23:55:54 +0200

    ENH: NAV-131 - Extend ConnectionQueryConfig with more query options.

--- src/main/java/ch/naviqore/service/config/ConnectionQueryConfig.java
+++ src/main/java/ch/naviqore/service/config/ConnectionQueryConfig.java
@@ -1,8 +1,11 @@
 package ch.naviqore.service.config;
 
+import ch.naviqore.service.TravelMode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+
+import java.util.EnumSet;
 
 @RequiredArgsConstructor
 @Getter
@@ -13,5 +16,8 @@ public class ConnectionQueryConfig {
     private final int minimumTransferDuration;
     private final int maximumTransferNumber;
     private final int maximumTravelTime;
+    private final boolean wheelchairAccessible;
+    private final boolean bikeAllowed;
+    private final EnumSet<TravelMode> travelModes;
 
 }
