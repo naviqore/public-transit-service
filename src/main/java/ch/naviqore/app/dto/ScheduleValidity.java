06cbd7c0830c073fdcc82c89535b8c867d8478fa
Lukas Connolly <lukas_connolly@hotmail.com>
2024-08-21 22:07:33 +0200

    ENH: NAV-131 - Add getters to scheduleValidity.

--- src/main/java/ch/naviqore/app/dto/ScheduleValidity.java
+++ src/main/java/ch/naviqore/app/dto/ScheduleValidity.java
@@ -1,12 +1,14 @@
 package ch.naviqore.app.dto;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+@Getter
 public class ScheduleValidity {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     final LocalDate startDate;
