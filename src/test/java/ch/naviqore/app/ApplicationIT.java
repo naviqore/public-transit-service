a3d5ec2be7d699ccfa7503737ab02b4174c0d6f2
Merlin Unterfinger <info@munterfinger.ch>
2024-10-15 23:07:16 +0200

    REFACTOR: 140 - Encapsulate GET request logic in TestRestRequest helper class

--- src/test/java/ch/naviqore/app/ApplicationIT.java
+++ src/test/java/ch/naviqore/app/ApplicationIT.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +13,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationIT {
 
+    @Autowired
+    private TestRestRequest request;
+
     @LocalServerPort
     private int port;
 
-    @Autowired
-    private TestRestTemplate template;
-
-    private String baseUrl() {
-        return "http://localhost:" + port;
-    }
-
     @Test
     void contextLoads() {
-        assertThat(template).isNotNull();
+        assertThat(request).isNotNull();
     }
 
     @Nested
@@ -34,37 +29,71 @@ class ApplicationIT {
 
         @Test
         void nearestStops_shouldReturn200() {
-            String url = baseUrl() + "/schedule/stops/nearest?latitude=36&longitude=-116&maxDistance=500000";
-            ResponseEntity<String> response = template.getForEntity(url, String.class);
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/schedule/stops/nearest")
+                    .addQueryParameter("latitude", "36")
+                    .addQueryParameter("longitude", "-116")
+                    .addQueryParameter("maxDistance", "500000")
+                    .get(String.class);
+
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         }
 
         @Test
         void autocompleteStopName_shouldReturn200() {
-            String url = baseUrl() + "/schedule/stops/autocomplete?query=Sta&searchType=STARTS_WITH&limit=100";
-            ResponseEntity<String> response = template.getForEntity(url, String.class);
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/schedule/stops/autocomplete")
+                    .addQueryParameter("query", "Sta")
+                    .addQueryParameter("searchType", "STARTS_WITH")
+                    .addQueryParameter("limit", "100")
+                    .get(String.class);
+
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         }
 
         @Test
         void autocompleteStopName_invalidSearchType_shouldReturn400() {
-            String url = baseUrl() + "/schedule/stops/autocomplete?query=Sta&searchType=NOT_A_TYPE&limit=100";
-            ResponseEntity<String> response = template.getForEntity(url, String.class);
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/schedule/stops/autocomplete")
+                    .addQueryParameter("query", "Sta")
+                    .addQueryParameter("searchType", "NOT_A_TYPE")
+                    .addQueryParameter("limit", "100")
+                    .get(String.class);
+
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         }
 
         @Test
         void departureAtStop_shouldReturn200() {
-            String url = baseUrl() + "/schedule/stops/STAGECOACH/departures?departureDateTime=2010-01-01T01:10:01&limit=3";
-            ResponseEntity<String> response = template.getForEntity(url, String.class);
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/schedule/stops/STAGECOACH/departures")
+                    .addQueryParameter("departureDateTime", "2010-01-01T01:10:01")
+                    .addQueryParameter("limit", "3")
+                    .get(String.class);
+
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         }
 
         @Test
         void departureAtStop_invalidStopId_shouldReturn404() {
-            String url = baseUrl() + "/schedule/stops/NOT_EXISTING/departures?departureDateTime=2010-01-01T01:10:01&limit=3";
-            ResponseEntity<String> response = template.getForEntity(url, String.class);
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/schedule/stops/NOT_EXISTING/departures")
+                    .addQueryParameter("departureDateTime", "2010-01-01T01:10:01")
+                    .addQueryParameter("limit", "3")
+                    .get(String.class);
+
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        }
+
+        @Test
+        void departureAtStop_outsideScheduleValidity_shouldReturn400() {
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/schedule/stops/STAGECOACH/departures")
+                    .addQueryParameter("departureDateTime", "2000-01-01T01:10:01")
+                    .addQueryParameter("limit", "3")
+                    .get(String.class);
+
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -73,29 +102,76 @@ class ApplicationIT {
 
         @Test
         void connectionBetweenStops_shouldReturn200() {
-            String url = baseUrl() + "/routing/connections?sourceStopId=STAGECOACH&targetStopId=NANAA&dateTime=2010-01-01T01:10:01";
-            ResponseEntity<String> response = template.getForEntity(url, String.class);
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/routing/connections")
+                    .addQueryParameter("sourceStopId", "STAGECOACH")
+                    .addQueryParameter("targetStopId", "NANAA")
+                    .addQueryParameter("dateTime", "2010-01-01T01:10:01")
+                    .get(String.class);
+
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         }
 
         @Test
+        void connection_invalidMinTransferTime_shouldReturn400() {
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/routing/connections")
+                    .addQueryParameter("sourceStopId", "STAGECOACH")
+                    .addQueryParameter("targetStopId", "NANAA")
+                    .addQueryParameter("dateTime", "2010-01-01T01:10:01")
+                    .addQueryParameter("minTransferTime", "-1")
+                    .get(String.class);
+
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        }
+
+        @Test
         void connection_invalidSourceStopId_shouldReturn404() {
-            String url = baseUrl() + "/routing/connections?sourceStopId=NOT_EXISTING&targetStopId=NANAA&dateTime=2010-01-01T01:10:01";
-            ResponseEntity<String> response = template.getForEntity(url, String.class);
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/routing/connections")
+                    .addQueryParameter("sourceStopId", "NOT_EXISTING")
+                    .addQueryParameter("targetStopId", "NANAA")
+                    .addQueryParameter("dateTime", "2010-01-01T01:10:01")
+                    .get(String.class);
+
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        }
+
+        @Test
+        void connection_invalidTargetStopId_shouldReturn404() {
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/routing/connections")
+                    .addQueryParameter("sourceStopId", "STAGECOACH")
+                    .addQueryParameter("targetStopId", "NOT_EXISTING")
+                    .addQueryParameter("dateTime", "2010-01-01T01:10:01")
+                    .get(String.class);
+
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         }
 
         @Test
         void isolineFromStop_shouldReturn200() {
-            String url = baseUrl() + "/routing/isolines?sourceStopId=STAGECOACH&maxTransferNumber=3&maxTravelTime=7200&dateTime=2010-01-01T01:10:01";
-            ResponseEntity<String> response = template.getForEntity(url, String.class);
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/routing/isolines")
+                    .addQueryParameter("sourceStopId", "STAGECOACH")
+                    .addQueryParameter("maxTransferNumber", "3")
+                    .addQueryParameter("maxTravelTime", "7200")
+                    .addQueryParameter("dateTime", "2010-01-01T01:10:01")
+                    .get(String.class);
+
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         }
 
         @Test
         void isoline_invalidSourceStopId_shouldReturn404() {
-            String url = baseUrl() + "/routing/isolines?sourceStopId=NOT_EXISTING&maxTransferNumber=3&maxTravelTime=7200&dateTime=2010-01-01T01:10:01";
-            ResponseEntity<String> response = template.getForEntity(url, String.class);
+            ResponseEntity<String> response = request.setPort(port)
+                    .setEndpoint("/routing/isolines")
+                    .addQueryParameter("sourceStopId", "NOT_EXISTING")
+                    .addQueryParameter("maxTransferNumber", "3")
+                    .addQueryParameter("maxTravelTime", "7200")
+                    .addQueryParameter("dateTime", "2010-01-01T01:10:01")
+                    .get(String.class);
+
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         }
     }
